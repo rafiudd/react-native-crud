b87a0ea1da1d39b177ce167a48ea55f3fb6f722a
rafiudd <sirin6867@gmail.com>
2019-12-09 10:05:20 +0700

    initial commit

---
+++ css/style.js
@@ -0,0 +1,31 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    width: 100 + '%',
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mt_2: {
+    marginTop: 24,
+  },
+  my_1: {
+    marginHorizontal: 12,
+  },
+  titleSize: {
+    fontSize: 50,
+  },
+  bgBlack: {
+    backgroundColor: '#29291e',
+  },
+  colorWhite: {
+    color: '#fff'
+  },
+  placeholderColor: {
+    color: '#adb5bd',
+  }
+});
